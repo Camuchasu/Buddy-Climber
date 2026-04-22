ee1a387b7a71a7ec387eecd8d070a73647bbb4bd
Camuchasu <24051908@d.anabuki-college.net>
2026-04-22 11:18:17 +0900

    プレイヤー作成段階（初期）
    初期の初期

---
+++ Assets/Matsumura/Scripct/PlayerBase.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    //[SerializeField] private float m_moveSpeed = 3.0f;
+    //[SerializeField] private float m_jumpPower = 3.0f;
+}
