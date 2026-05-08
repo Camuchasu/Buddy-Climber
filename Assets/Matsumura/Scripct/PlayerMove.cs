97b116759baf9c25380c206b1ab9d5b298f2d286
Camuchasu <24051908@d.anabuki-college.net>
2026-05-08 12:27:56 +0900

    Playerの地面の設置設定
    あんまし変わっていない

---
+++ Assets/Matsumura/Scripct/PlayerMove.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("Layer")]
+    public LayerMask groundLayer;
+
+    [Header("Player Size")]
+    public float playerHeight = 2.0f;
+
+    [Header("Ground Check")]
+    public float groundCheckRadius = 0.25f;
+    public float groundCheckDistance = 0.1f;
+
+    [Header("Ceiling Check")]
+    public float ceilingCheckRadius = 0.25f;
+    public float ceilingCheckDistance = 0.1f;
+
+    private bool isGrounded;
+    private bool isCeiling;
+
+    void Update()
+    {
+        CheckGround();
+        CheckCeiling();
+
+        if (isGrounded)
+        {
+            Debug.Log("接地中");
+        }
+
+        if (isCeiling)
+        {
+            Debug.Log("頭上ヒット");
+        }
+    }
+
+    void CheckGround()
+{
+    Vector3 origin =
+        transform.position +
+        Vector3.down * (playerHeight * 0.5f - groundCheckRadius);
+
+    if (Physics.SphereCast(
+        origin,
+        groundCheckRadius,
+        Vector3.down,
+        out RaycastHit hit,
+        groundCheckDistance,
+        groundLayer))
+    {
+        // 面の向きを確認
+        float angle = Vector3.Angle(hit.normal, Vector3.up);
+
+        // 角度が小さいなら地面
+        isGrounded = angle < 45f;
+    }
+    else
+    {
+        isGrounded = false;
+    }
+}
+    void CheckCeiling()
+    {
+        Vector3 origin =
+            transform.position +
+            Vector3.up * (playerHeight * 0.5f - ceilingCheckRadius);
+
+        isCeiling = Physics.SphereCast(
+            origin,
+            ceilingCheckRadius,
+            Vector3.up,
+            out RaycastHit hit,
+            ceilingCheckDistance,
+            groundLayer
+        );
+
+        Debug.DrawRay(
+            origin,
+            Vector3.up * ceilingCheckDistance,
+            Color.blue
+        );
+    }
+}
